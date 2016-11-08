3ba2db9b34270c2158600d839fe1528e818072db
cR0Ck <crockok@gmail.com>
2016-11-08 23:16:09 +0100

    - Aim logging now tells the position of each tank 'Tank x aiming 0,0,0 from 1,1,1'

--- BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
@@ -44,5 +44,6 @@ void UTankAimingComponent::TickComponent( float DeltaTime, ELevelTick TickType, 
 // Public Methods
 void UTankAimingComponent::AimAt(FVector HitLocation) {
 	auto OurTankName = GetOwner()->GetName();
-	UE_LOG(LogTemp, Warning, TEXT("%s aiming at %s"), *OurTankName, *HitLocation.ToString());
+	auto BarrelLocation = Barrel->GetComponentLocation().ToString();
+	UE_LOG(LogTemp, Warning, TEXT("%s aiming at %s from %s"), *OurTankName, *HitLocation.ToString(), *BarrelLocation);
 }
--- BattleTank/Source/BattleTank/Tank.cpp
+++ BattleTank/Source/BattleTank/Tank.cpp
@@ -42,3 +42,8 @@ void ATank::SetupPlayerInputComponent(class UInputComponent* InputComponent)
 void ATank::AimAt(FVector HitLocation) {
 	TankAimingComponent->AimAt(HitLocation);
 }
+
+void ATank::SetBarrelReference(UStaticMeshComponent* BarrelToSet) {
+	TankAimingComponent->SetBarrelReference(BarrelToSet);
+}
+
