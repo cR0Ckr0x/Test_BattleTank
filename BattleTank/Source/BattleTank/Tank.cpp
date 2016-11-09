2fddb1e6830113304de55498876322f8067cd3ca
cR0Ck <crockok@gmail.com>
2016-11-09 21:11:01 +0100

    Some code cleaning and fix (remove tick in some areas where not needed) + Logging if any Aim solution is found OR not

--- BattleTank/Source/BattleTank/Tank.cpp
+++ BattleTank/Source/BattleTank/Tank.cpp
@@ -8,7 +8,7 @@
 ATank::ATank()
 {
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
 
 	// No need to protect pointer as added at construction
 	// I am creating an inherited TankAimingComponent called "Aiming Component"
@@ -22,13 +22,6 @@ void ATank::BeginPlay()
 {
 	Super::BeginPlay();
 	
-}
-
-// Called every frame
-void ATank::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-
 }
 
 // Called to bind functionality to input
