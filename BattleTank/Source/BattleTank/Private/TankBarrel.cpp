8ed4f9caae405a278b6248638acf527714515a5c
cR0Ck <crockok@gmail.com>
2016-11-10 12:44:27 +0100

    - Barrel now correctly elevates at a possible rate, from 0° to 40°

--- BattleTank/Source/BattleTank/Private/TankBarrel.cpp
+++ BattleTank/Source/BattleTank/Private/TankBarrel.cpp
@@ -8,6 +8,14 @@ void UTankBarrel::Elevate(float RelativeSpeed) {
 	// Move the barrel the right amount this frame
 	// Given a max elevation speed, and the frame time
 
+	RelativeSpeed = FMath::Clamp<float>(RelativeSpeed, -1, +1);
+
+	
+	auto ElevationChange = RelativeSpeed * MaxDegreesPerSecond * GetWorld()->DeltaTimeSeconds; // with DeltaTimeSeconds I make the elevation frame-rate independent
+	auto RawNewElevation = RelativeRotation.Pitch + ElevationChange;
+	auto Elevation = FMath::Clamp<float>(RawNewElevation, MinElevationDegrees, MaxElevationDegrees);
+
+	SetRelativeRotation(FRotator(Elevation, 0, 0));
 }
 
 
