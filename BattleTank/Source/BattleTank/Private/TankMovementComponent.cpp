126d8bf0d9e01a3597c803685c76b7fcd1d25f93
cR0Ck <crockok@gmail.com>
2016-11-15 00:40:19 +0100

    - AI Tanks Finally move (and rotate) towards us! Intercept mode

--- BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
@@ -13,14 +13,24 @@ void UTankMovementComponent::Initialise(UTankTrack* LeftTrackToSet, UTankTrack* 
 void UTankMovementComponent::RequestDirectMove(const FVector& MoveVelocity, bool bForceMaxSpeed) {
 	// No need to call Super as we're replacing the functionality
 
+	// 1. Faccio avanzare back/forward AI Tank
 	auto TankForward = GetOwner()->GetActorForwardVector().GetSafeNormal();
 	auto AIForwardIntention = MoveVelocity.GetSafeNormal();
-
+	// Vector DOT product
 	//					  (Magnitude of A * Mag of B * cos of angle between A and B)
 	// Dot Product: A*B = ||A||*||B||*cos(Theta)
-	// (Represents the Parallellness of 2 vector. If 2 vectors are parallel (theta=0), then it's A*B and it's at maximum - if vectors are perpendicolar (theta=90), then cos(90)=0 and dot product is 0)
-	auto ForwardThrow = FVector::DotProduct(AIForwardIntention, TankForward);
+	// (Represents the Parallellness of 2 vectors. If 2 vectors are parallel (theta=0), then it's A*B and it's at maximum - if vectors are perpendicolar (theta=90), then cos(90)=0 and dot product is 0)
+	auto ForwardThrow = FVector::DotProduct(TankForward, AIForwardIntention);
 	IntendMoveforward(ForwardThrow);
+	// 2. Ruoto AI Tank verso la direzione in cui deve andare
+
+	// Vector CROSS product
+	// A x B == ||A||*||B||*sen(theta)*n
+	// Theta is the angle between A and B in the plane containing them (so between 0° and 180°
+	// n is a unit vector prependicular to the plane
+	// (Represents "PERPENDICULARITY" of 2 vectors. Maximum when vectors are perpendicular (sen(90°)=1), zero when they are parallel (sen(0°)=0 => Vector = A*B*0 = 0). Gives Always a Vector orthogonal to both. 
+	FVector ForwardRight = FVector::CrossProduct(TankForward, AIForwardIntention);
+	IntendTurnRight(ForwardRight.Z);
 	UE_LOG(LogTemp, Warning, TEXT("%s: move velocity: %s"), *(GetOwner()->GetName()), *(AIForwardIntention.ToString()));
 }
 
